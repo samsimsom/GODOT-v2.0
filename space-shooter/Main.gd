38b51520359ec7959c2959209d2f6d7b6148b632
sam sim som <samsimsom@gmail.com>
2021-09-23 23:25:48 +0300

    player

--- space-shooter/Main.gd
+++ space-shooter/Main.gd
@@ -5,5 +5,11 @@ func _ready() -> void:
 	print("{node_name} - Ready!".format({"node_name": self.name}))
 
 
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEvent:
+		if event.is_action_pressed("quit_game"):
+			get_tree().quit()
+
+
 func _process(_delta: float) -> void:
 	pass
